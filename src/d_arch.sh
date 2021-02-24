84d5922ebbce4013d887cd6a963e1f9cb991e138
Li Huang <lihuang.dmft@gmail.com>
2021-02-24 22:36:10 +0800

    update d_arch.sh

--- src/d_arch.sh
+++ src/d_arch.sh
@@ -10,15 +10,16 @@
 ##
 ## The name of the output archive should like this:
 ##     iqist_43e2cbb_1441276643.tar.gz
-## Here 43e2cbb is the abbreviated commit hash, and 1441276643 is the
-## UNIX timestamp when this commit was committed.
+## Here `43e2cbb` is the abbreviated commit hash, `1441276643` is the
+## UNIX timestamp when this commit was committed, and `iqist` is the code
+## name which is provided by the users.
 ##
 ## This script should be used by the developer only.
 ##
 ## Usage
 ## =====
 ##
-## ./d_arch.sh
+## ./d_arch.sh code_name
 ##
 ## Author
 ## ======
@@ -31,10 +32,11 @@
 ## =======
 ##
 ## 02/06/2015 by li huang (created)
-## 06/05/2017 by li huang (last modified)
+## 02/24/2021 by li huang (last modified)
 ##
 ##
 
+echo "Generating archive for $1"
 short_hash_tag=`git show -s --format='%h_%at' HEAD`
-archive_name=iqist_$short_hash_tag.tar.gz
+archive_name=$1_$short_hash_tag.tar.gz
 git archive -o $archive_name HEAD
