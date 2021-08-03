1ad71a6453d4198e3190afcbb108ef895ec71abf
Li Huang <lihuang.dmft@gmail.com>
2021-08-03 10:18:09 +0800

    update d_rpath.sh

--- src/d_rpath.sh
+++ src/d_rpath.sh
@@ -1,1 +1,21 @@
-install_name_tool -add_rpath /opt/intel/compilers_and_libraries_2017.0.102/mac/mkl/lib $1
+#!/bin/bash
+
+##
+##
+## Author
+## ======
+##
+## This shell script is designed, created, and maintained by
+##
+## Li Huang // email: lihuang.dmft@gmail.com
+##
+## History
+## =======
+##
+## 08/03/2021 by li huang (created)
+## 08/03/2021 by li huang (last modified)
+##
+##
+
+MKL=/opt/intel/compilers_and_libraries_2017.0.102/mac/mkl/lib
+install_name_tool -add_rpath $MKL $1
