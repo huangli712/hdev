2960129ed186a1a518dae0d705479ccb5b750e7d
Li Huang <lihuang.dmft@gmail.com>
2021-08-03 09:58:57 +0800

    update hdev

--- src/d_check.py
+++ src/d_check.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python
 
 ##
-##
-## Introduction
-## ============
-##
-## It is a python script. The purpose of this script is to check whether
-## the lines in a given file are ended with blanks.
-##
-## This script should be used by the developer only. It is only compatible
-## with Python 3.x.
-##
-## Usage
-## =====
-##
-## ./d_check.py file_name
-##
 ## Author
 ## ======
 ##
