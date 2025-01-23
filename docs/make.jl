31de63d7607f9515283dae29a69d946aaa69a156
Li Huang <lihuang.dmft@gmail.com>
2025-01-23 09:55:53 +0800

    fix make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,14 +3,18 @@ using Documenter
 makedocs(
     sitename="hdev",
     clean = false,
-    authors = "Li Huang",
+    authors = "Li Huang <huangli@caep.cn> and contributors",
     format = Documenter.HTML(
         prettyurls = false,
         ansicolor = true,
+        repolink = "https://github.com/huangli712/hdev",
+        size_threshold = 409600, # 400kb
+        assets = ["assets/hdev.css"],
     ),
+    remotes = nothing,
     pages = [
         "Home" => "index.md",
-        "User's guide" => Any[
+        "User's Guide" => Any[
             "d_arch.sh"  => "guide/arch.md",
             "d_rpath.sh" => "guide/rpath.md",
             "d_sar.sh"   => "guide/sar.md",
